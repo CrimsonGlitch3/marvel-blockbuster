862d70e5549af713e4011410f35cffaaed30f2d4
CrimsonGlitch3 <itspandastic@gmail.com>
2026-08-08 13:32:18 +0200

    PA : L'actu calée sur la largeur de l'univers, ligne du bas équilibrée
    L'actu passe de .pacol-large (part flexible) à .pacol-actu fixée à
    270px, la largeur réelle de l'univers en ligne du haut. Affiliation
    perd son épinglage à 72px : avec l'actu sortie du calcul flexible,
    elle se partage le reste à égalité avec Staff et À l'honneur via le
    flex: 1 1 0 par défaut de .pacol (110px chacune).

--- PA.php
+++ PA.php
@@ -22,8 +22,10 @@
 		<div class="pacol">
 			<div class="pasub">L'univers</div>
 			<div class="patexte">Forum dérivé des films, séries et comics <span class="reflet">Marvel</span>. Les personnages inventés ne sont pas autorisés.</div>
+			<div class="patexte paligne"><f>Un vrai blockbuster</f> Action non stop, monde en danger et rythme frénétique !</div>
 			<div class="patexte paligne"><f>Forum éphémère</f> Trente jours, une histoire, une fin. C'est maintenant ou jamais !</div>
-			<div class="patexte paligne"><f>Priorité à l'action</f> Fiches courtes, RP courts, animations en continu.</div>
+			<div class="patexte paligne"><f>Priorité au jeu</f> Le staff à vos côtés : fiches express, RP courts, animations en continu.</div>
+			<a class="paplus paplus-droite" href="t22-le-contexte">En savoir plus →</a>
 		</div>
 
 	</div>
@@ -31,7 +33,7 @@
 	<!-- ///// LIGNE 2 : affiliation + staff + actu + à l'honneur ///// -->
 	<div class="parow">
 
-		<div class="pacol pacol-affil">
+		<div class="pacol">
 			<div class="pasub">Affiliation</div>
 			<div class="pacontenu paaffil">
 				<a href="https://www.pub-rpg-design.com/">PRD</a>
@@ -48,7 +50,7 @@
 						<a href="https://marvel-blockbuster.forumactif.com/u2"><img src="https://i.imgur.com/P8lmtug.png" alt="Pietro Maximoff" /></a>
 					</div>
 				</div>
-				<a class="paplus" href="">Rejoindre&nbsp;?</a>
+				<a class="paplus" href="">Recrutement</a>
 			</div>
 		</div>
 
@@ -60,12 +62,13 @@
 					<div class="pavisage"><a href=""><img src="https://i.imgur.com/c28b9wS.png" alt="" /></a></div>
 					<div class="pavisage"><a href=""><img src="https://i.imgur.com/c28b9wS.png" alt="" /></a></div>
 				</div>
-				<a class="paplus" href="">Recrutement</a>
+				<a class="paplus" href="">En savoir +</a>
 			</div>
 		</div>
 
-		<div class="pacol pacol-large">
+		<div class="pacol pacol-actu">
 			<div class="pasub">L'actu</div>
+			<div class="pacontenu paactu"><f>08/08</f> Ouverture du forum ! Première version du forum par <i>heretics</i>. Le lancement de <span class="reflet">l'Acte 1</span> est immiment !</div>
 			<div class="pacontenu paactu"><f>08/08</f> Ouverture du forum ! Première version du forum par <i>heretics</i>. Le lancement de <span class="reflet">l'Acte 1</span> est immiment !</div>
 		</div>
 
